dd16672a58d7269d76bc25088e375d270b705b13
Ishita <ib3609581@gmail.com>
2026-04-01 18:39:37 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import paymentRouter from './routes/paymentRoute.js';
 const app=express();
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://aiinterviewprep-client.onrender.com",
     credentials:true
 }))
 
